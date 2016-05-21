d247109f4457acc888edc8380b53b781783b8b80
osboxes <osboxes@osboxes>
2016-05-21 15:24:27 -0400

    Add inverse DFT function

--- main1.cpp
+++ main1.cpp
@@ -11,6 +11,7 @@
 
 void readIn(std::vector<double>& fillWithData, std::string fileName);
 std::vector<MathVector> DFT(std::vector<double> data);
+std::vector<double> IDFT(std::vector<MathVector> coeffs);
 
 
 int main() {
@@ -43,8 +44,15 @@ int main() {
 	std::mt19937_64 gen( std::time(NULL) );
 
 
-	std::cout << dist( gen );
+	std::cout << dist( gen ) << std::endl;
+
+
 	//rebuild curves then average
+	std::vector<double> inverse = IDFT(DFTData);
+
+	for (unsigned int i = 0; i < inverse.size(); i++){
+		std::cout << inverse[i] << std::endl;
+	}
 
 	//fit to original data
 
@@ -78,7 +86,7 @@ std::vector<MathVector> DFT(std::vector<double> data) {
 
 	int N = data.size();
 
-	std::vector<MathVector> DFT;
+	std::vector<MathVector> DFT_result;
 
 	for (int n=0; n<N; n++) {
 		MathVector sum(2, 0.0);
@@ -96,9 +104,40 @@ std::vector<MathVector> DFT(std::vector<double> data) {
 			complex.clear();
 		}
 
-		DFT.push_back(sum);
+		DFT_result.push_back(sum);
 		sum.clear();
 	}
 
-	return DFT;
+	return DFT_result;
+}
+
+
+
+std::vector<double> IDFT(std::vector<MathVector> coeffs) {
+
+	int N = coeffs.size();
+
+	std::vector<double> IDFT_result;
+
+	for (int n=0; n<N; n++) {
+		double sum = 0.0;
+
+		for (int m=0; m<N; m++){
+
+			sum += coeffs[m][0] * std::cos( (2*M_PI*m*n)/N ) ;
+			sum += - coeffs[m][1] * std::sin( (2*M_PI*m*n)/N ) ;
+
+			//std::cout << complex[0] << " , " << complex[1] << std::endl;
+			//std::cout << ( complex * inData[m] )[1] << std::endl;
+			//std::cout << sum[0] << " , " << sum[1] << std::endl;
+		}
+
+		IDFT_result.push_back(sum);
+	}
+
+	for (unsigned int i = 0; i < IDFT_result.size(); i++){
+		IDFT_result[i] *= (1.0 / N);
+	}
+
+	return IDFT_result;
 }
